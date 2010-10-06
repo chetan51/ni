02ab195485571232ec7e33d84c7f424775a9afba
Chetan Surpur <chetan51@gmail.com>
2010-10-06 16:37:03 -0700

    Fixed minor spelling error in comment

--- example/controllers/home.js
+++ example/controllers/home.js
@@ -1,5 +1,5 @@
 /*
- * 	HomeControllers - a controller to be used with Ni that is called by
+ * 	HomeController - a controller to be used with Ni that is called by
  *	the router when a user visits the root URL, /.
  *
  *	When the root URL, /, is loaded, the index function below is called.
