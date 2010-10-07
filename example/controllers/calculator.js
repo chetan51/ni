d03cf0a16e9b5a4c86b3cad5e33adcad11bf5034
Chetan Surpur <chetan51@gmail.com>
2010-10-07 00:31:39 -0700

    Replaced all tabs with spaces and fixed some alignment

--- example/controllers/calculator.js
+++ example/controllers/calculator.js
@@ -1,73 +1,73 @@
 /*
- * 	CalculatorController - a controller to be used with Ni that is called by
- *	the router when a user visits a URL that starts with /calculator.
+ *  CalculatorController - a controller to be used with Ni that is called by
+ *  the router when a user visits a URL that starts with /calculator.
  *
- *	Controllers are represented as Javascript objects, and exported as a Node
- *	module after the object definition.
- *	
- *	Functions in controllers are called with the request and result as arguments
- *	(just like Connect functions are called) and then with the rest of the
- *	segments of the URL as the rest of the arguments.
+ *  Controllers are represented as Javascript objects, and exported as a Node
+ *  module after the object definition.
+ *  
+ *  Functions in controllers are called with the request and result as arguments
+ *  (just like Connect functions are called) and then with the rest of the
+ *  segments of the URL as the rest of the arguments.
  *
- *	For example, loading the URL /calculator/add/4/5 would call the add function
- *	below with a = 4 and b = 5.
+ *  For example, loading the URL /calculator/add/4/5 would call the add function
+ *  below with a = 4 and b = 5.
  *
- *	If no function is specified in the URL (such as the URL /calculator), then
- *	then index function is called if it exists.
+ *  If no function is specified in the URL (such as the URL /calculator), then
+ *  then index function is called if it exists.
  *
- *	Note: The req argument gives you access to the request, and you can use the
- *	res argument to send back a response to the browser or requester.
+ *  Note: The req argument gives you access to the request, and you can use the
+ *  res argument to send back a response to the browser or requester.
  */
 
 /*
- * 	Module dependencies
+ *  Module dependencies
  */
 
 var Ni = require('../../lib/ni'),
-	Mu = require('mu'),
-	Quip = require('quip');
+    Mu = require('mu'),
+    Quip = require('quip');
 
 /*
- *	The calculator controller
+ *  The calculator controller
  */
 
 var CalculatorController = function() {
-	
-	/*
-	 * 	This function is called when the URL does not indicate a function to
-	 * 	be called, so it would look like /calculator.
-	 */
+    
+    /*
+     *  This function is called when the URL does not indicate a function to
+     *  be called, so it would look like /calculator.
+     */
 
-	this.index = function(req, res) {
-		res.ok('Welcome to the calculator!');
-	}
+    this.index = function(req, res) {
+        res.ok('Welcome to the calculator!');
+    }
 
-	/*
-	 * 	This function is called when the URL indicates "add" as the function
-	 *	to be called, so it would look like /calculator/add.
-	 */
-	
-	this.add = function(req, res, a, b) {
-		if (a && b) {
-			a = parseInt(a);
-			b = parseInt(b);
+    /*
+     *  This function is called when the URL indicates "add" as the function
+     *  to be called, so it would look like /calculator/add.
+     */
+    
+    this.add = function(req, res, a, b) {
+        if (a && b) {
+            a = parseInt(a);
+            b = parseInt(b);
 
-			var template = Ni.views.calculator.template;
-			var data = {result: a + b};
+            var template = Ni.views.calculator.template;
+            var data = {result: a + b};
 
-			var compiled = Mu.compileText(template, null);
-			compiled(data).addListener('data', function (c) {
-				res.ok(c);
-			});
-		}
-		else {
-			res.error("a and b must both be provided.");
-		}
-	}
+            var compiled = Mu.compileText(template, null);
+            compiled(data).addListener('data', function (c) {
+                res.ok(c);
+            });
+        }
+        else {
+            res.error("a and b must both be provided.");
+        }
+    }
 };
 
 /*
- * 	Exports the calculator controller
+ *  Exports the calculator controller
  */
 
 module.exports = new CalculatorController();
